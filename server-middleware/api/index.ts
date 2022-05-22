811c67c1c0e6a15431029545773dd9b7f732989b
Buga Adrian Alexandru <bugaadrianalexandru@gmail.com>
2022-05-22 13:49:04 +0300

    data model in express

--- server-middleware/api/index.ts
+++ server-middleware/api/index.ts
@@ -3,11 +3,42 @@ import express, { Express, Request, Response } from 'express';
 
 // Initialize the express engine
 const app: Express = express();
- 
+
 app.use(express.json())
 
 app.get('/getJSON', (req: Request, res: Response) => {
-  res.json({ data: 'data' })
+  res.json([
+    {
+      date: '2022-05-22T00:00:00.000Z',
+      events: [
+        {
+          id: 1,
+          title: "Excursie",
+          description: "Mergem la munte",
+          startDate: '2022-05-22T10:00:00.000Z',
+          endDate: '2022-05-23T12:00:00.000Z',
+          hasNext: true,
+          priority: 100,
+          colorIndex: 1
+        }
+      ]
+    },
+    {
+      date: '2022-05-23T00:00:00.000Z',
+      events: [
+        {
+          id: 1,
+          title: "Excursie",
+          description: "Mergem la munte",
+          startDate: '2022-05-22T10:00:00.000Z',
+          endDate: '2022-05-23T12:00:00.000Z',
+          hasNext: false,
+          priority: 100,
+          colorIndex: 1
+        }
+      ]
+    }
+  ])
 })
 
 export default {
